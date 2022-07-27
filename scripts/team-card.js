c9f59f1332f3d89225302bf1f828bd2ee11e9d3d
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-07-27 20:07:07 +0100

    Complete HTML, CSS and JavaScript to make a responsive site with the Team mate session created dynamically using JavaScript

--- scripts/team-card.js
+++ scripts/team-card.js
@@ -1,41 +1,41 @@
-const team = document.getElementsByClassName("the-team");
+const team = document.getElementById("the-team-container");
 
-teams = [
+ const teams = [
     {
         imageBackground : "../images/speakers/chess.png",
-        image: "",
+        image: "../images/speakers/stanley.jpg",
         teamHead: "Nnamani Stanley Wisdom",
         teamSubHead: "Senior Software Developer",
-        teamDescription: ""
-    }
+        teamDescription: "Stanley is the CEO and founder of I-Health Company. A multibillion dollar company"
+    },
     {
         imageBackground : "../images/speakers/chess.png",
         image: "../images/speakers/gates.jpg",
         teamHead: "Bill Gates",
         teamSubHead: "He is business magnate philanthropist and co-founder of Microsoft",
         teamDescription: "Bill Gates has held the Forbes title of the richest person in the world for the longest period."
-    }
+    },
     {
         imageBackground : "../images/speakers/chess.png",
         image: "../images/speakers/Dangote.jpg",
         teamHead: "Aliko Dangote",
         teamSubHead: "He is a billionaire business magnate.CEO of the Dangote Group.",
         teamDescription: "Dangote became Nigeria's first billionaire in 2007."
-    }
+    },
     {
         imageBackground : "../images/speakers/chess.png",
         image: "../images/speakers/oprah.jpg",
         teamHead: "Oprah Gail Winfrey",
         teamSubHead: "Oprah Winfrey an American talk , actress, author, and philanthropist.",
         teamDescription: "Dubbed the Queen of All Media, was once the world's only black billionaire."
-    }
+    },
     {
         imageBackground : "../images/speakers/chess.png",
         image: "../images/speakers/diane.jpg",
         teamHead: "Diane Marie Hendricks",
         teamSubHead: "She is billionaire businesswoman and film producer from Wisconsin",
         teamDescription: "She has served on the boards"
-    }
+    },
     {
         imageBackground : "../images/speakers/chess.png",
         image: "../images/speakers/mars.jpg",
@@ -43,5 +43,29 @@ teams = [
         teamSubHead: "She is an American billionaire heiress and businesswoman.",
         teamDescription: "She has served on several boards."
     }
-]
+];
 
+var teamContent = "";
+
+for (let index= 0; index<teams.length; index++)
+{
+    teammate = teams[index];
+    var htmlContent = `
+    <div class="team-container">
+        <div class="team-image-container">
+            <img class="team-background" src="${teammate.imageBackground}" alt="">
+            <img class="team-image" src="${teammate.image}" alt="">
+        </div>
+        <div class="team-text-container">
+            <h2 class="teammate-name">${teammate.teamHead}</h2>
+            <h3 class="teammate-role">${teammate.teamSubHead}</h3>
+            <p class="teammate-description">${teammate.teamDescription}</p>
+        </div>
+    </div>
+    `;
+
+    teamContent += htmlContent;
+}
+
+
+team.innerHTML = teamContent;
